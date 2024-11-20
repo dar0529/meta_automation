9baec865eb7fc051d18cbb8fe2f6d827868d727c
Darwin Borjal <10015340@globetel.com>
2024-11-20 15:35:12 +0800

    added ni darwin

--- sample.py
+++ sample.py
@@ -1,9 +1,9 @@
 print("Hello World!")
-<<<<<<< HEAD
+
 print("sample push")
-=======
+
 
 print("Hello Dar")
->>>>>>> 149a0c8d2d2cf2149d0fb8bc8e24c396eb3cb324
 
 print("Hello Me!")
+print("Sampele Pusssssssh!")

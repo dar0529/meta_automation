1b60611c37e76b95688813d769ac9a49dc91c13f
Jesrel Alvarado <10012884@globetel.com>
2024-11-20 14:03:40 +0800

    sample jes

--- sample_jes.py
+++ sample_jes.py
@@ -1,9 +1,1 @@
-print("Hello World!")
-# <<<<<<< HEAD
-print("sample push")
-# =======
-
-print("Hello Dar")
-# >>>>>>> 149a0c8d2d2cf2149d0fb8bc8e24c396eb3cb324
-
-# sample
+#i woke up from the dream
